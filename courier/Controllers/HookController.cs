7bb988a047bf32671370a8478d807c6f331ef355
= <=>
2026-04-03 17:43:20 +0700

    init healthcheck

---
+++ courier/Controllers/HookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace courier.Controllers;
+
+public class HookController : Controller
+{
+    [HttpGet(Name = "HealthCheck")]
+    public IActionResult Index()
+    {
+        return Ok(new {status="Fine"});
+    }
+}
